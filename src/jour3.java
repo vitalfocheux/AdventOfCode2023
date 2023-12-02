4c7fbce8d30421559973cdc3ea3d09fbbb7d7cfb
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2023-12-02 18:41:32 +0100

    Initialisation pour Jour 3

---
+++ src/jour3.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class jour3 {
+
+    static void part1(){
+
+        List<String> parser = AOC.parser("/txt/jour2.txt", "\n");
+
+        for(String str : parser){
+
+        }
+
+        
+        System.out.println();
+    }
+
+    static void part2(){
+
+        List<String> parser = AOC.parser("/txt/jour2.txt", "\n");
+
+        for(String str : parser){
+            
+        }
+        
+        System.out.println();
+
+    }
+    
+    public static void main(String[] args) throws Exception{
+        System.out.print("Première partie : ");
+        part1();
+        System.out.print("Seconde partie : ");
+        part2();
+    }
+}
